d0b383d9a85e654b2f85499570ce5e55d12f0699
Karsten Rabe <karstenrabe91@gmail.com>
2019-10-24 00:21:22 -0400

    Add some content, links, start of talk.

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,44 @@
 <script>
+  import Links from "./Components/Links.svelte";
+
   // export let name;
 </script>
 
 <style>
+  #logo {
+    height: 45px;
+    width: auto;
+  }
+  header > h1 {
+    display: inline-block;
+  }
   main {
     text-align: center;
+  }
+  li {
+    list-style-type: none;
+    padding-bottom: 8px;
   }
 </style>
 
 <main>
-  <h1>Welcome to Svelte.js!</h1>
+  <header>
+    <img src="./assets/images/svelte-logo.png" alt="Svelte logo" id="logo" />
+    <h1>Welcome to Svelte.js!</h1>
+  </header>
+  <hr />
+  <p>What is Svelte?</p>
+  <ul>
+    <li>
+      Created by
+      <a href="https://github.com/Rich-Harris">Rich Harris</a>
+      , who is also the creator of
+      <a href="https://rollupjs.org/guide/en/">Rollup</a>
+    </li>
+    <li>"Svelte is a radical new approach to building user interfaces."</li>
+    <li>A "lightweight modern JavaScript library (4.57KB minified)"</li>
+    <li>A compiler of sorts</li>
+  </ul>
 </main>
+
+<Links />
---
+++ src/Components/Links.svelte
@@ -0,0 +1,35 @@
+<script>
+
+</script>
+
+<style>
+
+</style>
+
+<div>Obligatory links</div>
+<ul>
+  <li>
+    <a href="https://svelte.dev/">Svelte developer page</a>
+  </li>
+  <li>
+    <a href="http://sveltejs.com/">Svelte homepage</a>
+  </li>
+  <li>
+    <a
+      href="https://css-tricks.com/what-i-like-about-writing-styles-with-svelte/">
+      CSS tricks article about Svelte
+    </a>
+  </li>
+  <li>
+    <a
+      href="https://medium.com/free-code-camp/a-realworld-comparison-of-front-end-frameworks-with-benchmarks-2019-update-4be0d3c78075">
+      A benchmark article of modern web frameworks
+    </a>
+  </li>
+  <li>
+    <a href="http://vanilla-js.com/">Vanilla JS</a>
+  </li>
+  <li>
+    <a href="https://bundlephobia.com/">Bundlephobia</a>
+  </li>
+</ul>
