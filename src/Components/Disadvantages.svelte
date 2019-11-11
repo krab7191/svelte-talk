ec36afe36e1a23d2dab68e1b2aed96d0b4a59ef8
Karsten Rabe <karstenrabe91@gmail.com>
2019-11-11 00:00:03 -0500

    Add prism and hack to get <code> to display properly

--- src/Components/Disadvantages.svelte
+++ src/Components/Disadvantages.svelte
@@ -1,8 +1,13 @@
+<script>
+  const code = "<code>";
+</script>
+
 <ul>
   <li>
     Newer, still evolving ecosystem: Maybe not the best choice for huge
     projects.
   </li>
   <li>IE support is shaky at best. IE 11 requires polyfills</li>
+  <li>Issue inserting a {code} block.</li>
 
 </ul>
--- src/Pages/State.svelte
+++ src/Pages/State.svelte
@@ -1,6 +1,23 @@
 <script>
   import Footer from "../Components/Footer.svelte";
+  let code = `<script>\n  import Header from "../Components/Header.svelte";\n  const name ="Karsten";\n<`;
+  code = code + `/script>\n\n<styl`;
+  code = code + `e>\n  p {\n    color: blue;\n  }\n</style>\n\n`;
+  code =
+    code +
+    `<Header />\n<main id="main-content">\n  <p>A svelte component</p>\n  <p>My name is {name}</p>\n</main>`;
 </script>
 
-<main id="main-content" />
+<style>
+
+</style>
+
+<main id="main-content">
+  <h2>The Svelte File</h2>
+  <div>
+    <pre>
+      <code class="language-js">{code}</code>
+    </pre>
+  </div>
+</main>
 <Footer />
