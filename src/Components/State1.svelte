00e1ecb3ef2d205d2a10468f8c5ec0279861e2cd
Karsten Rabe <karstenrabe91@gmail.com>
2019-11-12 00:05:22 -0500

    Style and show code output

---
+++ src/Components/State1.svelte
@@ -0,0 +1,16 @@
+<script>
+  import Header from "./Header.svelte";
+  const name = "Karsten";
+</script>
+
+<style>
+  p {
+    color: purple;
+  }
+</style>
+
+<Header />
+<main id="main-content">
+  <p>A svelte component</p>
+  <p>My name is {name}</p>
+</main>
--- src/Pages/State.svelte
+++ src/Pages/State.svelte
@@ -1,10 +1,11 @@
 <script>
   import { onMount } from "svelte";
   import Prism from "prismjs";
+  import State1 from "../Components/State1.svelte";
   import Footer from "../Components/Footer.svelte";
   let code = `<script>\n  import Header from "../Components/Header.svelte";\n  const name ="Karsten";\n<`;
   code = code + `/script>\n\n<styl`;
-  code = code + `e>\n  p {\n    color: blue;\n  }\n</style>\n\n`;
+  code = code + `e>\n  p {\n    color: purple;\n  }\n</style>\n\n`;
   code =
     code +
     `<Header />\n<main id="main-content">\n  <p>A svelte component</p>\n  <p>My name is {name}</p>\n</main>`;
@@ -14,15 +15,20 @@
 </script>
 
 <style>
-
+  div.code-box {
+    margin: 0 auto;
+    width: 50%;
+    max-width: 680px;
+  }
 </style>
 
 <main id="main-content">
   <h2>The Svelte File</h2>
-  <div>
+  <div class="code-box">
     <pre>
       <code class="language-js">{code}</code>
     </pre>
+    <State1 />
   </div>
 </main>
 <Footer />
