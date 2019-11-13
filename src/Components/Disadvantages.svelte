9833302d1bb442295487f00bd5e06ae9e9c7750b
Karsten Rabe <karstenrabe91@gmail.com>
2019-11-12 23:22:12 -0500

    Tweak CSS statements

--- src/Components/Disadvantages.svelte
+++ src/Components/Disadvantages.svelte
@@ -16,11 +16,17 @@
     Newer, still evolving ecosystem: Maybe not the best choice for huge
     'enterprise' projects.
   </li>
-  <li>IE support is shaky at best. IE 11 requires polyfills</li>
+  <li>IE support is weak. IE 11 requires polyfills. (Who cares)</li>
   <li>
-    Issues parsing string literals and
+    Svelte Parser issues with string literals and
     <code>{code}</code>
     blocks.
   </li>
-  <li>Dynamic CSS must be done inline.</li>
+  <li>
+    No JS in CSS, dynamic CSS must be done inline.
+    <a
+      href="https://css-tricks.com/what-i-like-about-writing-styles-with-svelte/#article-header-id-7">
+      BUT CSS variables allow for a hack.
+    </a>
+  </li>
 </ul>
